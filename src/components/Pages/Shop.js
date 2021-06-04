fa17e5c74f0bbccdcbca091d66050811a1b1cd20
Tim Jackson <timjacksonm@gmail.com>
2021-06-04 17:06:22 -0500

    Drills.js Updates -Added images for 4 drills. -Object created for each drill. -export from file and import into Shop.js

--- src/components/Pages/Shop.js
+++ src/components/Pages/Shop.js
@@ -4,6 +4,7 @@ import Footer from '../Footer/Footer';
 import hammers from '../Inventory/Hammers';
 import circularSaws from '../Inventory/Saws';
 import toolbags from '../Inventory/Toolbags';
+import drills from '../Inventory/Drills';
 
 export default function Shop() {
   console.log(hammers);
@@ -11,8 +12,17 @@ export default function Shop() {
     <div className='flex flex-col text-center h-screen bg-default-pattern bg-cover overflow-auto items-center'>
       <Nav />
       <main className='flex flex-grow justify-center items-center'>
-        {toolbags.map(
-          ({ category, id, brand, name, price, description, previewImage }) => {
+        {drills.map(
+          ({
+            category,
+            id,
+            brand,
+            name,
+            price,
+            description,
+            previewImage,
+            gallery,
+          }) => {
             return (
               <div>
                 <img src={previewImage} alt='' />
