0fc008582fb23a7c9aa2d2fbb2befa710fc550d7
Tim Jackson <timjacksonm@gmail.com>
2021-07-11 12:42:41 -0500

    fixes -gave delete button a better size. -changed let variable to const. -fixed screen width on mobile when no items in cart.

--- src/components/Pages/Cart.js
+++ src/components/Pages/Cart.js
@@ -7,6 +7,7 @@ function Cart() {
   const [cart, setCart] = useState(Storage.retrieveData());
   const [cartPrice, setCartPrice] = useState();
   const quanityPerItem = cart.reduce((acc, currentValue) => {
+    //Sorts cart array for multiples and gives item quanity data.
     if (!acc[currentValue.id]) {
       acc[currentValue.id] = 0;
     }
@@ -25,7 +26,7 @@ function Cart() {
 
   useEffect(() => {
     if (cart.length > 1) {
-      let price = cart
+      const price = cart
         .map((item) => item.price)
         .reduce((a, b) => Number(a) + Number(b))
         .toFixed(2);
@@ -40,29 +41,30 @@ function Cart() {
     Storage.storeData(cart);
   }, [cartPrice, cart]);
 
-  let newArray = [];
+  const duplicates = [];
 
   return (
     <div className='bg-default-pattern bg-cover h-screen'>
       <NavBar cart={cart} />
-      <div className='absolute top-20 h-full p-2 bg-white rounded overflow-auto md:h-5/6 md:top-28 md:right-0 md:left-0 md:mx-48'>
+      <div className='absolute top-20 h-full p-2 left-0 right-0 bg-white rounded overflow-auto md:h-5/6 md:top-28 md:right-0 md:left-0 md:mx-48'>
         <div className='m-4 text-lg font-bold underline'>Shopping Cart</div>
         {cart.map((item) => {
-          if (newArray.includes(item.id)) {
+          if (duplicates.includes(item.id)) {
+            //Checks array for duplicates.
             return null;
           }
-          newArray.push(item.id);
+          duplicates.push(item.id);
           return (
             <div
               key={item.id}
               className='mx-4 my-1 p-2 border-2 flex flex-col items-center md:items-stretch'
             >
-              <div className='flex justify-between'>
-                <div className='my-1'>{`${item.brand} ${item.name}`}</div>
+              <div className='flex justify-between w-full'>
+                <div className='my-1 w-5/6'>{`${item.brand} ${item.name}`}</div>
                 <TiDelete
                   onClick={deleteItem}
                   id={item.id}
-                  size='2em'
+                  size='30px'
                   className='hover:text-yellow-300'
                 />
               </div>
